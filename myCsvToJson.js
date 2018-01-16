89ca44a881abb7a5c5ae06861fbc4e2bc722541b
Aziz Boudi <azicode4@gmail.com>
2018-01-15 17:02:18 -0800

    Aziz boudi submission of Assignment 1 CSV to JSON Converter

---
+++ myCsvToJson.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const csv = require('csvtojson')
+const csvFilePath = './customer-data.csv'
+
+const convertCsvToJson = (theCsvFilePath) => {
+  // create a json variable where the converted csv will be pushed
+  const myJson =[];
+
+  csv()
+    .fromFile(theCsvFilePath)
+    .on('json', (jsonObj) => {
+      // push converted csv to the array json
+      myJson.push(jsonObj)
+      fs.writeFileSync(('customer-data.json'), JSON.stringify(myJson))
+    })
+    .on('done', (error) => {
+      if (error) throw error
+      console.log(`end csv has been converted into JSON file and has been saved in customer-data.json`)
+    })
+}
+
+convertCsvToJson(csvFilePath);
